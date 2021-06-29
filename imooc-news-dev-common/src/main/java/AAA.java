1ef722eb8e3cadeae52c73e24a3937d7dd361311
bie <1062505123@qq.com>
2021-06-29 15:29:23 +0800

    111

--- imooc-news-dev-common/src/main/java/AAA.java
+++ imooc-news-dev-common/src/main/java/AAA.java
@@ -7,5 +7,6 @@ public class AAA {
     public static void main(String[] args) {
         System.out.println("33333333333");
         System.out.println("44444444");
+        System.out.println("6666666");
     }
 }
