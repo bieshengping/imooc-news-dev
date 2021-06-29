1700112d45e64425403e9859b3c3ea1111830f2e
bie <1062505123@qq.com>
2021-06-29 15:25:06 +0800

    111

---
+++ imooc-news-dev-common/src/main/java/AAA.java
@@ -0,0 +1,11 @@
+/**
+ * @author: bie
+ * @create: 2021-06-29 14:29
+ */
+public class AAA {
+
+    public static void main(String[] args) {
+        System.out.println("33333333333");
+        System.out.println("44444444");
+    }
+}
